ca1b212ca216423788e9cec9e877ee133a872506
Hugo Orielso <orielso.lozano15@gmail.com>
2024-05-18 15:54:15 -0500

    final

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import express from 'express'
-
-
-
 const PORT = process.env.PORT ?? 4321
 const app = express()
 
 app.use("/", express.static('dist', {redirect:false}))
-console.log(process.cwd() + "/dist/images/secciones/arquitectonica");
 app.get("*", (req,res,next)=>{
     res.sendFile(process.cwd() + '/dist/index.html')
 })
