ae758ec2b6fd384c637dfb16eba5914bad00e82c
Hugo Orielso <orielso.lozano15@gmail.com>
2024-05-22 18:36:54 -0500

    dark mode added

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const PORT = process.env.PORT ?? 4321
 const app = express()
 
 app.use("/", express.static('dist', {redirect:false}))
+
 app.get("*", (req,res,next)=>{
     res.sendFile(process.cwd() + '/dist/index.html')
 })
-
 
 app.listen(PORT,()=>{
     console.log(`Server on port ${PORT}`);
